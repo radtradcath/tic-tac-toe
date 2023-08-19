a482609ccc1e694dbea2cd0adba5f29a1d0b679c
radtradcath <vinicius.b.bonafini@gmail.com>
2023-08-19 17:08:40 -0300

    Create displayers and reset button fuctionality

--- javascript.js
+++ javascript.js
@@ -16,8 +16,6 @@ let playerFactory = function (name, mark) {
 };
 
 function GameController() {
-    let player1 = playerFactory('player1', 'X');
-    let player2 = playerFactory('player2', 'O');
     let turn = 0;
     let playerTurn;
 
@@ -27,7 +25,7 @@ function GameController() {
         }
     }
 
-    function handleTurn() {
+    function handleTurn(player1, player2) {
         if (turn % 2 == 0) {
             playerTurn = player1.mark;
             turn++;
@@ -51,12 +49,14 @@ function GameController() {
             ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[5] === marker) && (Gameboard.gameboard[8] === marker)) ||
             ((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[8] === marker)) ||
             ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[6] === marker))) {
-            
-            return `${marker} wins the round!`;
+
+            return marker;
 
         } else if (Gameboard.gameboard.indexOf(null) === -1) {
-            console.log('That\' a tie');
             return 'That\' a tie';
+        }
+        else {
+            return;
         }
     }
     return {
@@ -70,6 +70,40 @@ function GameController() {
 function DisplayController() {
     let cells = document.querySelectorAll('.cell');
     let newGame = GameController();
+    let btnGetName = document.querySelector('#getNames');
+    let inputName1 = document.querySelector('#username1');
+    let inputName2 = document.querySelector('#username2');
+    let displayTurn = document.querySelector('.display-turn');
+    let displayWinner = document.querySelector('.display-winner');
+    let resetBtn = document.querySelector('.reset');
+    let firstPlayer;
+    let secondPlayer;
+
+    function handleNamesEvent(e) {
+        let player1Name = inputName1.value;
+        let player2Name = inputName2.value;
+
+        firstPlayer = playerFactory(player1Name, 'X');
+        secondPlayer = playerFactory(player2Name, 'O');
+        e.preventDefault();
+        displayTurn.textContent = `${firstPlayer.name}'s turn`;
+    }
+
+    function updateDisplayTurn() {
+        displayTurn.textContent == `${firstPlayer.name}'s turn` ? displayTurn.textContent = `${secondPlayer.name}'s turn` :
+            displayTurn.textContent == `${secondPlayer.name}'s turn` ? displayTurn.textContent = `${firstPlayer.name}'s turn` : "";
+    }
+
+    function checkEndGame(winner) {
+        if (newGame.checkWinner(winner) === 'X') {
+            displayWinner.textContent = `${firstPlayer.name} is the winner!`
+            displayTurn.textContent = "";
+        } else if (newGame.checkWinner(winner) === 'O') {
+            displayWinner.textContent = `${secondPlayer.name} is the winner!`
+            displayTurn.textContent = "";
+        }
+    }
+
 
     function renderGameboard() {
         for (i = 0; i <= Gameboard.gameboard.length - 1; i++) {
@@ -78,21 +112,35 @@ function DisplayController() {
     }
 
     function handleGameEvent(e) {
-        let playerTurn = newGame.handleTurn();
-
-        if (e.target.textContent !== "") {
+        if (e.target.textContent !== "" || !firstPlayer || !secondPlayer || firstPlayer.name === undefined || secondPlayer.name === undefined) {
             return void (0);
 
         } else {
+            let playerTurn = newGame.handleTurn(firstPlayer, secondPlayer);
             newGame.chooseCell(e.target.id, playerTurn);
-            renderGameboard();
-            newGame.checkWinner(playerTurn);
+            renderGameboard();            
+            updateDisplayTurn();
+            checkEndGame(playerTurn);
         }
+    }
+
+    function resetGame() {
+        displayTurn.textContent = "";
+        displayWinner.textContent = "";
+        inputName1.value = "";
+        inputName2.value = "";
+        Gameboard.clearBoard();
+        firstPlayer.name = undefined;
+        secondPlayer.name = undefined;
     }
 
     cells.forEach((c) => {
         c.addEventListener('click', handleGameEvent);
     });
+
+    btnGetName.addEventListener('click', handleNamesEvent);
+
+    resetBtn.addEventListener('click', resetGame);
 
     renderGameboard();
 
