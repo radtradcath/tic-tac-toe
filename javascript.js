76d487a64eea5e752314e74f9298c0b1c7d405bc
radtradcath <vinicius.b.bonafini@gmail.com>
2023-08-19 14:03:20 -0300

    Organize code in modules

--- javascript.js
+++ javascript.js
@@ -1,71 +1,104 @@
-let Gameboard = (() => {
-    let gameboard = [null, null, null, null, null, null, null, null, null];
-    let turn = 0;
-    return {
-        gameboard,
-        turn
-    }
-})();
+let Gameboard = {
+    gameboard: [null, null, null, null, null, null, null, null, null],
 
+    clearBoard: function () {
+        let render = DisplayController();
+        Gameboard.gameboard = [null, null, null, null, null, null, null, null, null];
+        render.renderGameboard();
+    }
+};
 
 let playerFactory = function (name, mark) {
-   let chooseCell = function(cell) {
-     if (!Gameboard.gameboard[cell]) {
-        return Gameboard.gameboard[cell] = this.mark;
-    }
-}
     return {
-        chooseCell,
         name,
         mark,
     };
 };
 
-let player1 = playerFactory('player1', 'X');
-let player2 = playerFactory('player2', 'O');
+function GameController() {
+    let player1 = playerFactory('player1', 'X');
+    let player2 = playerFactory('player2', 'O');
+    let turn = 0;
+    let playerTurn;
 
+    function chooseCell(cell, playerMark) {
+        if (!Gameboard.gameboard[cell]) {
+            Gameboard.gameboard[cell] = playerMark;
+        }
+    }
 
-let renderGameboard = function () {    
-    for (i = 0; i <= Gameboard.gameboard.length - 1; i++) {
-        cells[i].textContent = Gameboard.gameboard[i];
+    function handleTurn() {
+        if (turn % 2 == 0) {
+            playerTurn = player1.mark;
+            turn++;
+            console.log(playerTurn);
+            return playerTurn;
+
+        } else {
+            playerTurn = player2.mark;
+            console.log(playerTurn);
+            turn++
+            return playerTurn;
+        }
+    }
+
+    function checkWinner(marker) {
+        if (((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[1] === marker) && (Gameboard.gameboard[2] === marker)) ||
+            ((Gameboard.gameboard[3] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[5] === marker)) ||
+            ((Gameboard.gameboard[6] === marker) && (Gameboard.gameboard[7] === marker) && (Gameboard.gameboard[8] === marker)) ||
+            ((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[3] === marker) && (Gameboard.gameboard[6] === marker)) ||
+            ((Gameboard.gameboard[1] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[7] === marker)) ||
+            ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[5] === marker) && (Gameboard.gameboard[8] === marker)) ||
+            ((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[8] === marker)) ||
+            ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[6] === marker))) {
+            
+            return `${marker} wins the round!`;
+
+        } else if (Gameboard.gameboard.indexOf(null) === -1) {
+            console.log('That\' a tie');
+            return 'That\' a tie';
+        }
+    }
+    return {
+        handleTurn,
+        chooseCell,
+        checkWinner,
     }
 }
 
-let playGame = function (e) {
-    if (e.target.textContent !== "") {
-        return void (0);
-    }
-    else if (Gameboard.turn % 2 == 0) {
-        player1.chooseCell(e.target.id);
-        Gameboard.turn++;
-        renderGameboard();
-        checkWinner('X');
-    } else {
-        player2.chooseCell(e.target.id);
-        Gameboard.turn++
-        renderGameboard();
-        checkWinner('O');
-    }
-}
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach((c) => {
-        c.addEventListener('click', playGame);
+function DisplayController() {
+    let cells = document.querySelectorAll('.cell');
+    let newGame = GameController();
+
+    function renderGameboard() {
+        for (i = 0; i <= Gameboard.gameboard.length - 1; i++) {
+            cells[i].textContent = Gameboard.gameboard[i];
+        }
+    }
+
+    function handleGameEvent(e) {
+        let playerTurn = newGame.handleTurn();
+
+        if (e.target.textContent !== "") {
+            return void (0);
+
+        } else {
+            newGame.chooseCell(e.target.id, playerTurn);
+            renderGameboard();
+            newGame.checkWinner(playerTurn);
+        }
+    }
+
+    cells.forEach((c) => {
+        c.addEventListener('click', handleGameEvent);
     });
 
-let checkWinner = function (marker) {
-    if (((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[1] === marker) && (Gameboard.gameboard[2] === marker)) ||
-        ((Gameboard.gameboard[3] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[5] === marker)) ||
-        ((Gameboard.gameboard[6] === marker) && (Gameboard.gameboard[7] === marker) && (Gameboard.gameboard[8] === marker)) ||
-        ((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[3] === marker) && (Gameboard.gameboard[6] === marker)) ||
-        ((Gameboard.gameboard[1] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[7] === marker)) ||
-        ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[5] === marker) && (Gameboard.gameboard[8] === marker)) ||
-        ((Gameboard.gameboard[0] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[8] === marker)) ||
-        ((Gameboard.gameboard[2] === marker) && (Gameboard.gameboard[4] === marker) && (Gameboard.gameboard[6] === marker))) {
+    renderGameboard();
 
-        console.log('you win')
-
-    } else if (Gameboard.gameboard.indexOf(null) === -1) {
-        console.log("That's a tie");
+    return {
+        renderGameboard,
     }
 }
+
+DisplayController();
